e4718f8ffbcc0f641c6c96a0bcbc4bb2580afa08
tdacunha <tdacunha@sas.upenn.edu>
2021-07-09 17:34:42 -0400

    Commit Friday, edits to flow_playground

--- flow_playground.py
+++ flow_playground.py
@@ -52,13 +52,17 @@ flow_callback.train(batch_size=batch_size, epochs=epochs, steps_per_epoch=steps_
 # plot learned distribution:
 N = 10000
 X_sample = np.array(flow_callback.dist_learned.sample(N))
+Y_sample = np.array(flow_callback.dist_gaussian_approx.sample(N))
 flow_chain = MCSamples(samples=X_sample, names=param_names, label='Learned distribution')
+Y_chain = MCSamples(samples=Y_sample, names=param_names, label='Transformed distribution')
+
 
 g = plots.get_subplot_plotter()
-g.triangle_plot([chain, flow_chain], params=param_names, filled=False)
+g.triangle_plot([chain, flow_chain, Y_chain], params=param_names, filled=False)
 
 # slice two dimensions:
 x = np.linspace(0.0, 2.0, 100)
+
 dir_1 = flow_callback.Z2X_bijector(np.vstack((x, np.zeros(len(x)))).T.astype(np.float32))
 dir_2 = flow_callback.Z2X_bijector(np.vstack((np.zeros(len(x)), x)).T.astype(np.float32))
 #dir_3 = flow_callback.Z2X_bijector(np.vstack((-x, np.zeros(len(x)))).T.astype(np.float32))
@@ -70,7 +74,7 @@ ax.plot(dir_1[:, 0], dir_1[:, 1])
 ax.plot(dir_2[:, 0], dir_2[:, 1])
 #ax.plot(dir_3[:, 0], dir_3[:, 1])
 
-# Trying to get inverted plot:
+# Inverted plot in abstract space:
 
 X2Z_bijector = tfb.Invert(flow_callback.Z2X_bijector)
 num_params = flow_callback.num_params
@@ -85,12 +89,64 @@ g = plots.get_subplot_plotter()
 g.triangle_plot([flow_chain1], params=['Z1', 'Z2'], filled=False)
 
 # slice two dimensions:
-x = np.linspace(0.0, 2.0, 100)
+#x = np.linspace(0.0, 2.0, 100)
 dir_1 = (np.vstack((x, np.zeros(len(x)))).T.astype(np.float32))
 dir_2 = (np.vstack((np.zeros(len(x)), x)).T.astype(np.float32))
 ax = g.subplots[1, 0]
 ax.plot(dir_1[:, 0], dir_1[:, 1])
 ax.plot(dir_2[:, 0], dir_2[:, 1])
+
+# Making gif
+# Inverted plot in abstract space:
+X2Z_bijector = tfb.Invert(flow_callback.Z2X_bijector)
+num_params = flow_callback.num_params
+dist_learned_inverted = tfd.TransformedDistribution(distribution=flow_callback.dist_learned, bijector=X2Z_bijector)
+
+#X_sample1 = np.array(flow_callback.dist_transformed.sample(N))
+Z_sample = np.array(dist_learned_inverted.sample(N))
+
+flow_chain1 = MCSamples(samples=Z_sample, names=['Z1', 'Z2'], label='Transformed distribution')
+
+
+
+# Plot in original space:
+
+
+
+x = np.linspace(0.0, 100.0, 500) #100
+#print(np.linspace(0,2*np.pi,8, endpoint = True))
+for n,theta in enumerate(np.linspace(0,2*np.pi,24, endpoint = True)):
+
+    vec_1 = np.vstack((x*np.cos(theta), -x*np.sin(theta))).T
+    vec_2 = np.vstack((x*np.sin(theta), x*np.cos(theta))).T
+
+    dir_1 = vec_1.astype(np.float32)
+    dir_2 = vec_2.astype(np.float32)
+    g1 = plots.get_subplot_plotter(subplot_size = 5)
+    g1.plot_2d([flow_chain1], param1='Z1', param2 = 'Z2', filled=False)
+
+    ax1 = g1.subplots[0,0]
+    ax1.plot(dir_1[:, 0], dir_1[:, 1])
+    ax1.plot(dir_2[:, 0], dir_2[:, 1])
+    #g1.export('/Users/TaraD/Desktop/ParameterGif/ZPlot_%s' % (n))
+
+
+    dir_1 = flow_callback.Z2X_bijector(vec_1.astype(np.float32))
+    dir_2 = flow_callback.Z2X_bijector(vec_2.astype(np.float32))
+    #dir_3 = flow_callback.Z2X_bijector(np.vstack((-x, np.zeros(len(x)))).T.astype(np.float32))
+    g = plots.get_subplot_plotter(subplot_size = 5)
+    g.plot_2d([chain, flow_chain], param1=param_names[0], param2 = param_names[1], filled=False)
+    ax = g.subplots[0,0]
+    ax.plot(dir_1[:, 0], dir_1[:, 1])
+    ax.plot(dir_2[:, 0], dir_2[:, 1])
+
+    g1.export('/Users/TaraD/Desktop/ParameterGif/ZPlot_%s.png' % (n))
+
+    g.export('/Users/TaraD/Desktop/ParameterGif/XPlot_%s.png' % (n))
+
+
+
+
 
 #g = plots.get_subplot_plotter()
 #g.triangle_plot([chain, flow_chain], params=param_names, filled=False)
